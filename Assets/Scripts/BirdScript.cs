b0c9f3fc13178ae0ed6d91855481f8569ee789cb
Sammy Lizon <lizon_sammy@yahoo.co.uk>
2024-05-03 02:00:03 +0100

    Player controller and obstacles

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float jumpSpeed; 
+
     // Start is called before the first frame update
     void Start()
     {
-        print("Sammy Lizon");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+        }
+
+       
     }
 }
